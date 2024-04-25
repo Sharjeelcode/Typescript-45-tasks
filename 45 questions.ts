3bcd52907b07fab963e159a6c4cdb81cd9524a25
unknown <sharjeelahmed3808@gmail.com>
2024-04-24 18:05:58 -0700

    first commit

---
+++ 45 questions.ts
@@ -0,0 +1,40 @@
+// // Task #2
+// let personsName: string = "Sharjeel";
+// console.log(`Hello ${personsName}, would you like to learn some Python today?`);
+
+// // Task #3
+
+// let secondPersonName : string = "ahmed"
+// console.log(secondPersonName.toLocaleUpperCase());
+// console.log(secondPersonName.toLowerCase());
+
+// // Task #4
+// let quote : string = "Life is like riding a bicycle. To keep your balance, you must keep moving."
+// console.log(`Albert Einstein once said,"${quote}"`);
+
+// // Task #5
+// let famousPerson :string = "Albert Einstein "
+// console.log(`${famousPerson} once said,"${quote}"`);
+
+// Task #6
+
+// let famousPersonSpaces :string = "             A\nlbe\nrt \t\nEins\ntein                      "  
+// let famousPersonWitoutSpaces : string = famousPersonSpaces.replace(/\n/g  ,"") 
+
+// console.log(famousPersonSpaces);
+// console.log(famousPersonWitoutSpaces.replace("\t" , "").trim()); 
+
+// Task #7
+
+// console.log(5+3);
+// console.log(2*4);
+// console.log(12-4);
+// console.log(16/2);
+
+// Task #8
+
+const favNumber : number = 8
+if (favNumber === 4) {
+    console.log(`your fav number is ${favNumber}`);
+    
+}
